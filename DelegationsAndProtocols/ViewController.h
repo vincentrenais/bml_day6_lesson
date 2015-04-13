5cea7af4d83c63ccd3faead8ec98f680d3475b30
Vincent Renais <vincentrenais@gmail.com>
2015-04-13 10:31:23 -0400

    Initial Commit

---
+++ DelegationsAndProtocols/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DelegationsAndProtocols
+//
+//  Created by Vincent Renais on 2015-04-13.
+//  Copyright (c) 2015 Vincent Renais. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
